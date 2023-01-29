52651f9ff70df0073bcd21467cc34502d98c9bad
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2023-01-29 13:01:40 +0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,24 +2,28 @@
 
 if command -v go &> /dev/null; then
   echo "Go is already installed."
-else
-  # Update package list
-  sudo apt update
-  
-  # Install dependencies
-  sudo apt install build-essential -y
-  
-  cd $HOME
-  export ver="1.19.2"
-  sudo wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-  sudo rm -rf /usr/local/go
-  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-  sudo rm "go$ver.linux-amd64.tar.gz"
-  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  exit 0
 fi
-  # Check
+
+# Update package list
+sudo apt update
+
+# Install dependencies
+sudo apt install build-essential -y
+
+cd $HOME
+ver="1.19.2"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+
+# Add Go to PATH for all users
+sudo echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/environment
+
+# Check
 if command -v go &> /dev/null; then
   echo "Go installed successfully."
 else
-  echo "An error occurred while installing GO."
+  echo "An error occurred while installing Go."
 fi
