e42469b6b47434fbfe7a205bd2fd13aa8eb5f2db
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2023-01-29 12:43:58 +0300

    Create rust.sh

---
+++ rust.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if command -v rustc &> /dev/null; then
+  echo "Rust is already installed."
+else
+  # Update package list
+  sudo apt update
+
+  # Install dependencies
+  sudo apt install curl -y
+
+  # Download and install rustup
+  sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+  # Add rust to PATH
+  . $HOME/.cargo/env
+fi
