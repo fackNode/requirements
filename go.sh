833b5f90f79b8241a58240e4f75e4afbb5eb1715
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2023-01-29 12:51:45 +0300

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if command -v go &> /dev/null; then
+  echo "Go is already installed."
+else
+  # Update package list
+  sudo apt update
+  
+  # Install dependencies
+  sudo apt install build-essential -y
+  
+  cd $HOME
+  ver="1.19.2"
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+fi
+  # Check
+if command -v go &> /dev/null; then
+  echo "Go installed successfully."
+else
+  echo "An error occurred while installing GO."
+fi
